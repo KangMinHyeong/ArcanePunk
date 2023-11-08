4d6ecb22d70adca44b2d1c60113ec04e36d30340
KangMinHyeong <kkmmhh0450@naver.com>
2023-11-08 14:19:33 +0900

    test

--- Source/ArcanePunk/ArcanePunkCharacter.h
+++ Source/ArcanePunk/ArcanePunkCharacter.h
@@ -26,10 +26,28 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION(BlueprintPure)
+	bool IsAttack_A();
+
+	UFUNCTION(BlueprintPure)
+	bool IsAttack_B();
+
+	UFUNCTION(BlueprintPure)
+	bool IsSkill_Q();
+
+	UFUNCTION(BlueprintPure)
+	bool IsSkill_E();
+
 private:
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
 	void ZoomInOut(float AxisValue);
+	void Attack_typeA();
+	void Attack_typeB();
+	void Skill_typeQ();
+	void Skill_typeE();
+	void StartJog();
+	void EndJog();
 	
 private:
 	UPROPERTY(EditAnywhere)
@@ -49,5 +67,31 @@ private:
 	float ZoomCoefficient = 20.0f;
 
 	FVector PlayerVec = FVector::ZeroVector;
+
+	bool bCanMove = true;
+
+	bool bAttack_A = false;
+	bool bAttack_B = false;
+	bool bSkill_Q = false;
+	bool bSkill_E = false;
+
+	FTimerHandle Attack_ATimerHandle;
+	FTimerHandle Attack_BTimerHandle;
+	FTimerHandle Skill_QTimerHandle;
+	FTimerHandle Skill_ETimerHandle;
+
+	UPROPERTY(EditAnywhere)
+	float Attack_CastingTime = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float Skill_CastingTime = 1.0f;
+
+	UPROPERTY(EditAnywhere)
+	float JoggingSpeed = 700.0f;
+
+	float DefaultSpeed = 400.0f;
+
+	UPROPERTY(EditAnywhere)
+	class UParticleSystemComponent* Skill_Q_Effect;
 
 };
