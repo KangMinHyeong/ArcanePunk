7462b1f426b3f8b723ac652a15c444cf9d4aa5a8
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2024-07-28 19:14:58 +0900

    #166 피격 Montage 연결
    - EnymBaseAnimInstance 클래스에 피격 몽타주 변수 추가
    - 각 적의 애니메이션 블루프린트에서 피격 몽타주 연결
    - 적 Base 애니메이션 블루프린트에서 피격 애니메이션 재생 부분 삭제

--- Source/ArcanePunk/Private/Enemy/Enemy_CharacterBase.cpp
+++ Source/ArcanePunk/Private/Enemy/Enemy_CharacterBase.cpp
@@ -176,6 +176,8 @@ float AEnemy_CharacterBase::TakeDamage(float DamageAmount, FDamageEvent const &D
 		ManaDropComp->SpawnManaDrop(DamageCauser);
 	}
 
+	EnemyAnim->PlayHit_Montage();
+
     return DamageApplied;
 }
 
